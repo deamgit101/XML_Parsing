17f017b1a2a4202135987e2ef374ffe6d60034fd
Deam Git101 <deamgit101@gmail.com>
2016-05-14 21:09:31 -0700

    My debugging DOMXPath. Choose DIFFERENT element LEVEL: deal[1] and deal[2]

--- src/main/java/core/DOMXPathParser.java
+++ src/main/java/core/DOMXPathParser.java
@@ -14,10 +14,10 @@ public class DOMXPathParser {
 
 		String url = "http://learn2test.net/sdc.xml";
 
-		String xpath_element_01 = "//deals/deal/description";
-		String xpath_element_02 = "//deals/deal/impact-score";
-		String xpath_element_03 = "//deals/deal/price";
-		String xpath_element_04 = "//deals/deal/orig-price";
+		String xpath_element_01 = "//deals/deal[1]/name";
+		String xpath_element_02 = "//deals/deal[2]/name";
+		String xpath_element_03 = "//deals/deal[1]/price";
+		String xpath_element_04 = "//deals/deal[2]/price";
 
 		String xpath_attribute_01 = "//deals/@count";
 
@@ -43,10 +43,10 @@ public class DOMXPathParser {
 		String element_04 = xpath.compile(xpath_element_04).evaluate(d);
 		String attribute_01 = xpath.compile(xpath_attribute_01).evaluate(d);
 
-		System.out.println("description: " + element_01);
-		System.out.println("impact-score: " + element_02);
-		System.out.println("price: " + element_03);
-		System.out.println("orig-price: " + element_04);
+		System.out.println("name FIRST LEVEL: " + element_01);
+		System.out.println("name SECOND LEVEL: " + element_02);
+		System.out.println("price FIRST LEVEL: " + element_03);
+		System.out.println("price SECOND LEVEL: " + element_04);
 		System.out.println("Number of deals: " + attribute_01);
 
 	}
